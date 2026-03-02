79f79be5ee92d252a5a02797f27ae2931cfabd79
Melih <melih.topacoglu37@gmail.com>
2026-03-02 13:49:49 +0300

    first commit

---
+++ db/models/Prices.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+    domain: { type: mongoose.SchemaTypes.ObjectId, required: true, ref: "domains" },
+    company: { type: mongoose.SchemaTypes.ObjectId, required: true, ref: "companies" },
+    new_registration_fee: { type: mongoose.SchemaTypes.Decimal128 },
+    new_registration_badges: { type: [String] },
+    renewal_fee: { type: mongoose.SchemaTypes.Decimal128 },
+    renewal_badges: { type: [String] },
+    transfer_fee: { type: mongoose.SchemaTypes.Decimal128 },
+    transfer_badges: { type: [String] },
+    currency: { type: String, default: "USD" },
+    language: { type: String, default: "EN" },
+    is_active: { type: Boolean, default: true },
+    date: { type: Date, required: true }
+}, {
+    versionKey: false,
+    timestamps: {
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+    }
+});
+class Prices extends mongoose.Model { }
+
+schema.loadClass(Prices);
+module.exports = mongoose.model("prices", schema);
